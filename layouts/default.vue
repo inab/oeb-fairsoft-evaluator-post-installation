a3d3015f35f071f827398c301000613c2fbe9784
emarti2 <eva.martin@bsc.es>
2023-05-29 16:47:33 +0200

    added post intallation content in index

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app id="main">
+      <v-container fluid>
+          
+          <Nuxt />
+          
+      </v-container>
+  </v-app>
+</template>
+<style scoped>
+#main {
+  background-color: '#FAFAFA';
+}
+
+</style>
+<script>
+
+export default {
+  name: "postGitHubAppInstall",
+}
+</script>
